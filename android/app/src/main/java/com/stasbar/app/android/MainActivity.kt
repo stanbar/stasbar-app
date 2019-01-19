fa4e31f83aeb8c86e2fa1dcd90113a58bc32b388
stasbar <stasbar1995@gmail.com>
2019-01-19 02:04:59 +0100

    add toolbar add ic launcher
    Took 1 hour 40 minutes

--- android/app/src/main/java/com/stasbar/app/android/MainActivity.kt
+++ android/app/src/main/java/com/stasbar/app/android/MainActivity.kt
@@ -25,34 +25,21 @@
 package com.stasbar.app.android
 
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    val logoText = """
+   _____
+  / ___/
+ (__  )
+/____/"""
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-    }
+    tvLogo.text = logoText
+  }
 }
