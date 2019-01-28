ddc6b0495b70b5e022deef2ae5bd4a47eaf4ac9b
stasbar <stasbar1995@gmail.com>
2019-01-28 19:49:20 +0100

    add screens in macbook pro frame add my apps and my projects
    Took 1 hour 14 minutes

--- android/app/src/main/java/com/stasbar/app/android/widgets/AppView.kt
+++ android/app/src/main/java/com/stasbar/app/android/widgets/AppView.kt
@@ -2,6 +2,7 @@ package com.stasbar.app.android.widgets
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.text.SpannableString
 import android.text.method.LinkMovementMethod
@@ -9,12 +10,14 @@ import android.text.style.ForegroundColorSpan
 import android.text.style.URLSpan
 import android.text.style.UnderlineSpan
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import com.google.android.flexbox.FlexboxLayout
 import com.stasbar.app.android.R
 
 
@@ -24,17 +27,26 @@ class AppView : LinearLayout {
     MOBILE, DESKTOP
   }
 
+  val name: String
+  private val description: String
+  private val image: Drawable
+  private val platform: Platform
+  private val github: String?
+  private val googlePlay: String?
+  private val website: String?
+  private val appStore: String?
+
   constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
     orientation = LinearLayout.VERTICAL
     val params = context.obtainStyledAttributes(attrs, R.styleable.AppView, 0, 0)
-    val name = params.getString(R.styleable.AppView_name)
-    val description = params.getString(R.styleable.AppView_description)
-    val image = params.getDrawable(R.styleable.AppView_img)
-    val platform = Platform.values()[params.getInt(R.styleable.AppView_platform, 0)]
-    val github = params.getString(R.styleable.AppView_github)
-    val googlePlay = params.getString(R.styleable.AppView_googlePlay)
-    val website = params.getString(R.styleable.AppView_website)
-    val appStore = params.getString(R.styleable.AppView_appStore)
+    name = checkNotNull(params.getString(R.styleable.AppView_name)) { "Can not be empty" }
+    description = checkNotNull(params.getString(R.styleable.AppView_description)) { "Can not be empty" }
+    image = checkNotNull(params.getDrawable(R.styleable.AppView_img)) { "Can not be empty" }
+    platform = Platform.values()[params.getInt(R.styleable.AppView_platform, Platform.MOBILE.ordinal)]
+    github = params.getString(R.styleable.AppView_github)
+    googlePlay = params.getString(R.styleable.AppView_googlePlay)
+    website = params.getString(R.styleable.AppView_website)
+    appStore = params.getString(R.styleable.AppView_appStore)
     params.recycle()
     val descriptionBuilder = StringBuilder(description)
 
@@ -53,6 +65,7 @@ class AppView : LinearLayout {
     }
 
     LayoutInflater.from(context).inflate(R.layout.app_view, this, true)
+
     val tvName = getChildAt(0) as TextView
     val ivAppImage = getChildAt(1) as ImageView
     val tvDescription = getChildAt(2) as TextView
@@ -62,6 +75,15 @@ class AppView : LinearLayout {
     tvDescription.movementMethod = LinkMovementMethod.getInstance()
 
     ivAppImage.setImageDrawable(image)
+  }
+
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+    val lp = layoutParams as FlexboxLayout.LayoutParams
+    Log.d("AppView", "${platform.name} \nFlexBasicPercent ${lp.flexBasisPercent} \nmaxWidth ${lp.maxWidth}")
+//    lp.flexBasisPercent = if (platform == Platform.MOBILE) 0.5f else 1.0f
+//    lp.minWidth = 200
+    layoutParams = lp
   }
 
   fun linkSpannable(name: String, href: String): SpannableString {
