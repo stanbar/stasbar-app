89589a982c7e8f0436d6957db81a24dc866c6354
stasbar <stasbar1995@gmail.com>
2019-05-13 23:20:58 +0200

    change my age
    Took 33 minutes

---
+++ backend/src/main/resources/assets/precache-manifest.2a61b40314cb8511868067a11c193382.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0178c8cedd94d8d8ae170ece0902468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69eda78660109a55ef5f",
+    "url": "/static/css/2.52da1176.chunk.css"
+  },
+  {
+    "revision": "ee97da5b2004c95b9c74",
+    "url": "/static/css/main.3faeb14c.chunk.css"
+  },
+  {
+    "revision": "69eda78660109a55ef5f",
+    "url": "/static/js/2.e95c516f.chunk.js"
+  },
+  {
+    "revision": "ee97da5b2004c95b9c74",
+    "url": "/static/js/main.7ff0faa7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c6a5ccb146873f97672208a219d0f235",
+    "url": "/static/media/B612Mono-Bold.c6a5ccb1.ttf"
+  },
+  {
+    "revision": "4f47a5bd2087dcf98e9a2c8ce3ed1581",
+    "url": "/static/media/GoLureLongFramed.4f47a5bd.webp"
+  },
+  {
+    "revision": "e20bd3bff19eecf85e030460ec42ace7",
+    "url": "/static/media/GoLureLongFramed.e20bd3bf.png"
+  },
+  {
+    "revision": "256c7062127a96ab9f3f6679d6b242da",
+    "url": "/static/media/KeybaseLogo.256c7062.svg"
+  },
+  {
+    "revision": "ccecf37c4a754b8b0b7418fb3b986ab0",
+    "url": "/static/media/RaspberryWalletFramed.ccecf37c.webp"
+  },
+  {
+    "revision": "f9f589d5841363a565a3af83574d5cbb",
+    "url": "/static/media/RaspberryWalletFramed.f9f589d5.png"
+  },
+  {
+    "revision": "3ae582d506b04d1b6f32ef7aa64780b0",
+    "url": "/static/media/StackOverflowIcon.3ae582d5.svg"
+  },
+  {
+    "revision": "2c4365ee84b282e78841dca365eb6cbc",
+    "url": "/static/media/TaxLedgerFramed.2c4365ee.webp"
+  },
+  {
+    "revision": "7e25a9796b1c03f26c0ef86c868b982b",
+    "url": "/static/media/TaxLedgerFramed.7e25a979.png"
+  },
+  {
+    "revision": "7fc1e6f1fdb533b2cd8dae1702f1a485",
+    "url": "/static/media/TaxLedgerLogo.7fc1e6f1.svg"
+  },
+  {
+    "revision": "3e2427a73eda8379c257e2bccf4430a4",
+    "url": "/static/media/VapeToolLogo.3e2427a7.svg"
+  },
+  {
+    "revision": "41166ef3494a9b0a3ae050292ebbf578",
+    "url": "/static/media/VapeToolLongFramed.41166ef3.webp"
+  },
+  {
+    "revision": "8e23a94dedc0ee03a30e8a833ec1bc79",
+    "url": "/static/media/VapeToolLongFramed.8e23a94d.png"
+  },
+  {
+    "revision": "843ab7e0b15a4c527b8a77582cbbc387",
+    "url": "/static/media/Wallpapers4KLongFramed.843ab7e0.webp"
+  },
+  {
+    "revision": "8c15a6bd730d773b1e77bbe338de85ae",
+    "url": "/static/media/Wallpapers4KLongFramed.8c15a6bd.png"
+  },
+  {
+    "revision": "83bf14cdf6ee39326ea41dcebf6d8abb",
+    "url": "/static/media/WallpapersOneClickLongFramed.83bf14cd.webp"
+  },
+  {
+    "revision": "dc77aea29c0d848022ac56c7cabc6dc5",
+    "url": "/static/media/WallpapersOneClickLongFramed.dc77aea2.png"
+  },
+  {
+    "revision": "ad986fb98e1a3c76b48153ece1f44cde",
+    "url": "/static/media/WebsiteFramed.ad986fb9.png"
+  },
+  {
+    "revision": "bab27de6c28a1250fc0b931d8322b89f",
+    "url": "/static/media/WebsiteFramed.bab27de6.webp"
+  },
+  {
+    "revision": "16c18fde65a7b18af8441aab47ec71ce",
+    "url": "/static/media/WishesLongFramed.16c18fde.webp"
+  },
+  {
+    "revision": "224111de12ad537d93b57407f3512f55",
+    "url": "/static/media/WishesLongFramed.224111de.png"
+  },
+  {
+    "revision": "0599391c16bcaf0974d98b5d7024e7d3",
+    "url": "/static/media/roboto-mono-latin-100.0599391c.woff"
+  },
+  {
+    "revision": "d6c0b24cee74e82397928ba806ad25bb",
+    "url": "/static/media/roboto-mono-latin-100.d6c0b24c.woff2"
+  },
+  {
+    "revision": "09ccd02230bd8f7a5a2d504ddbceb21a",
+    "url": "/static/media/roboto-mono-latin-100italic.09ccd022.woff2"
+  },
+  {
+    "revision": "0c62fd54d535f2c86d5ea67a38a1bbb3",
+    "url": "/static/media/roboto-mono-latin-100italic.0c62fd54.woff"
+  },
+  {
+    "revision": "4f691391d5a34cc629a83e82091d5b3c",
+    "url": "/static/media/roboto-mono-latin-300.4f691391.woff"
+  },
+  {
+    "revision": "db95b37ec6ef04cd349d098941e5f70b",
+    "url": "/static/media/roboto-mono-latin-300.db95b37e.woff2"
+  },
+  {
+    "revision": "18357ddaddc4d4f13c5f49985327a2db",
+    "url": "/static/media/roboto-mono-latin-300italic.18357dda.woff2"
+  },
+  {
+    "revision": "8debb2d5f4dfc8732c97f38e47904cb9",
+    "url": "/static/media/roboto-mono-latin-300italic.8debb2d5.woff"
+  },
+  {
+    "revision": "1db1469bf7acbd08f97df4f18046a50f",
+    "url": "/static/media/roboto-mono-latin-400.1db1469b.woff"
+  },
+  {
+    "revision": "e92cc0fb9e1a7debc138224fd02a462a",
+    "url": "/static/media/roboto-mono-latin-400.e92cc0fb.woff2"
+  },
+  {
+    "revision": "55befc28b6c06f712a38e2f4153967e1",
+    "url": "/static/media/roboto-mono-latin-400italic.55befc28.woff2"
+  },
+  {
+    "revision": "5711b84f6506b02f585ba40860d4f1a0",
+    "url": "/static/media/roboto-mono-latin-400italic.5711b84f.woff"
+  },
+  {
+    "revision": "077525e5ca30c27e6709adf6ca296b48",
+    "url": "/static/media/roboto-mono-latin-500.077525e5.woff"
+  },
+  {
+    "revision": "f70f506c38700cacd79c9564cc98552c",
+    "url": "/static/media/roboto-mono-latin-500.f70f506c.woff2"
+  },
+  {
+    "revision": "93b351bdb383554dbe2db6c8e33a6101",
+    "url": "/static/media/roboto-mono-latin-500italic.93b351bd.woff"
+  },
+  {
+    "revision": "c5465264d55124f161cffe17e7790e7b",
+    "url": "/static/media/roboto-mono-latin-500italic.c5465264.woff2"
+  },
+  {
+    "revision": "4bc0bd04b8b2f6730b5997503fa4ce5a",
+    "url": "/static/media/roboto-mono-latin-700.4bc0bd04.woff2"
+  },
+  {
+    "revision": "f61e5a8dfaad2adb9479f8013b9bb101",
+    "url": "/static/media/roboto-mono-latin-700.f61e5a8d.woff"
+  },
+  {
+    "revision": "5eca10b5e58c4e2a8164f89bef5dbd7e",
+    "url": "/static/media/roboto-mono-latin-700italic.5eca10b5.woff2"
+  },
+  {
+    "revision": "7324e30e83c2d31fddc3281766f07943",
+    "url": "/static/media/roboto-mono-latin-700italic.7324e30e.woff"
+  }
+]);
