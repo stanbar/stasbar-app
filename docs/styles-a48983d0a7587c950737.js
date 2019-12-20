2fb09e71afdc47468df2cc436cda2a20de3b4dc7
Stanislaw Baranski <stachu@stasbar.com>
2019-12-20 15:09:49 +0100

    update page

--- docs/styles-a48983d0a7587c950737.js
+++ docs/styles-a48983d0a7587c950737.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"1LhQ":function(n,w,o){}}]);
-//# sourceMappingURL=styles-8dbe281b993d428aa255.js.map
+//# sourceMappingURL=styles-a48983d0a7587c950737.js.map
