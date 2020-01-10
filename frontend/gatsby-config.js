53e7b59934c6e28dfd641a1061e237dd06257ab6
Stanislaw Baranski <stachu@stasbar.com>
2020-01-10 13:38:28 +0100

    add gatsby-plugin-ipfs

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,16 +1,18 @@
 module.exports = {
+  pathPrefix: "__GATSBY_IPFS_PATH_PREFIX__",
   plugins: [
     "gatsby-plugin-top-layout",
     "gatsby-plugin-typescript",
     "gatsby-plugin-material-ui",
     "gatsby-plugin-react-helmet",
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: "gatsby-plugin-google-analytics",
       options: {
         // The property ID; the tracking code won't be generated without it
         trackingId: "UA-123888398-1"
       }
-    }
+    },
+    "gatsby-plugin-ipfs"
   ],
   siteMetadata: {
     title: "stasbar"
