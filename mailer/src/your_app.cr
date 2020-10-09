0f538a7b85ca3b5be94bcabd5eadf3413d6bec99
Stanislaw Baranski <stachu@stasbar.com>
2020-10-09 23:21:27 +0200

    add mailer

---
+++ mailer/src/your_app.cr
@@ -0,0 +1,17 @@
+# TODO: Write documentation for `YourApp`
+require "sendgrid"
+module YourApp
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+  client = Sendgrid::Client.new("https://api.sendgrid.com/v3/mail/send", ENV["SENDGRID_API_KEY"])
+  message = Sendgrid::Message.new
+  message.to << Sendgrid::Address.new(email="stasbar1995@gmail.com", name="test@example.com")
+  message.from = Sendgrid::Address.new(email="golden-nuggets@stasbar.com", name="Golden Nuggets")
+  message.subject = "Sending with SendGrid is Fun"
+  message.content = Sendgrid::Content.new(type: "text/plain", body: "and easy to do anywhere, even with Ruby")
+  send = client.send message
+  puts message.to_json
+  puts send.status_code
+  puts send.body
+end
